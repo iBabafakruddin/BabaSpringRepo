454eb93bd4bfcd371280c0d24d29b00fb7fdb6d0
Baba <iBabafakruddin@gmail.com>
2014-09-07 12:45:17 +0530

    controller created.

---
+++ SpringHibernate/src/com/baba/demo/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.baba.demo.controller;
+
+/**
+ * @author baba
+ * This is Spring Controller component (To be develop).
+ * 
+ */
+public class LoginController
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+        System.out.println("Welcome to Spring");
+    }
+
+}
